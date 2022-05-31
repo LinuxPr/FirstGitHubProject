eba24d07ccc0fd6a60ad74bac340e7cc1643e86e
root <root@5b4ad84f582c.mylabserver.com>
2022-05-31 03:16:36 +0000

    updated file

--- linux.sh
+++ linux.sh
@@ -9,7 +9,7 @@ echo "Your username is:" && whoami
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
-echo "Please wait while we process the date......."
+echo "Please and thank you for waiting  while we process the date......."
 sleep 3
 echo "Made on:" && date
 echo "-----------------------------"
